0f58e88fdddb43192202e5cf0ce5c447b547260b
Ivan Ponomarev <trollvan_work@mail.ru>
2019-01-29 23:44:05 +0100

    Init commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    {id: 11, name: 'Pornomarev'},
+    {id: 12, name: 'TrollVan'},
+    {id: 13, name: 'Erast.Petrovich'},
+    {id: 14, name: 'Margoz'},
+    {id: 15, name: 'Poet'},
+    {id: 16, name: 'SadClon'},
+    {id: 17, name: 'ChSV'},
+    {id: 18, name: 'Ponomarev'},
+    {id: 19, name: 'Secret'}
+];
